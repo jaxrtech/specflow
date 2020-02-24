8dbfba99a1b5dfce908a25ba35a61eff456f2ae4
Josh Bowden <josbow@gmail.com>
2020-02-24 00:26:41 -0600

    Updates

---
+++ precache-manifest.23e2f33cbf82f84f9536d838220cca4e.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6cec56f98013f7c27d2b92d2725cf31",
+    "url": "/specflow/index.html"
+  },
+  {
+    "revision": "65db390ee89037110e20",
+    "url": "/specflow/static/css/2.365cd67e.chunk.css"
+  },
+  {
+    "revision": "36bde85bcb9a1c3e9486",
+    "url": "/specflow/static/css/main.66a3f4eb.chunk.css"
+  },
+  {
+    "revision": "65db390ee89037110e20",
+    "url": "/specflow/static/js/2.4da357f2.chunk.js"
+  },
+  {
+    "revision": "41713ba8906e691cbb3a07624b0e5029",
+    "url": "/specflow/static/js/2.4da357f2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "36bde85bcb9a1c3e9486",
+    "url": "/specflow/static/js/main.4825057e.chunk.js"
+  },
+  {
+    "revision": "79b3aa8fb49420f7dbfd",
+    "url": "/specflow/static/js/runtime-main.bd82643a.js"
+  },
+  {
+    "revision": "03ab771b196ad90f28a35a5aa8f5ec08",
+    "url": "/specflow/static/media/icons-16.03ab771b.ttf"
+  },
+  {
+    "revision": "84c180eabdd692efd197345a33735e8c",
+    "url": "/specflow/static/media/icons-16.84c180ea.woff"
+  },
+  {
+    "revision": "9631870bf93f28bc1d7010bf55621ae8",
+    "url": "/specflow/static/media/icons-16.9631870b.eot"
+  },
+  {
+    "revision": "053082015844707d8048e606b922cdad",
+    "url": "/specflow/static/media/icons-20.05308201.ttf"
+  },
+  {
+    "revision": "5c61819764f1fa973dda8ef77e391451",
+    "url": "/specflow/static/media/icons-20.5c618197.woff"
+  },
+  {
+    "revision": "ca7934b30a0b9829dbeb0b58a6fd0b51",
+    "url": "/specflow/static/media/icons-20.ca7934b3.eot"
+  }
+]);
